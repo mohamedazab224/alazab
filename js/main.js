41e716af0b0b507001771154487da9f1ad77131a
mohamedazab224 <159525429+mohamedazab224@users.noreply.github.com>
2024-12-03 14:17:30 +0200

    Create main.js
    Signed-off-by: mohamedazab224 <159525429+mohamedazab224@users.noreply.github.com>

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// فتح القائمة المنبثقة للفيديو
+function openVideoPopup() {
+    document.getElementById("videoPopup").style.display = "flex";
+}
+
+// إغلاق القائمة المنبثقة للفيديو
+function closeVideoPopup() {
+    document.getElementById("videoPopup").style.display = "none";
+}
